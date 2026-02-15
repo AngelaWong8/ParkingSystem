07e2095cb9eec84adec503555270012eeacf2298
mmonchii <afia.fahrudean@gmail.com>
2026-02-16 01:51:41 +0800

    Update FixedFineStrategy.java

--- FinalCode/src/model/FixedFineStrategy.java
+++ FinalCode/src/model/FixedFineStrategy.java
@@ -1,9 +1,18 @@
 package model;
+
 public class FixedFineStrategy implements FineStrategy {
     @Override
     public double calculateFine(long overstayMinutes) {
-        return overstayMinutes > 0 ? 50.0 : 0.0;
+        // Only charge fine if there is actually an overstay (beyond 24 hours)
+        // overstayMinutes parameter should be the minutes beyond the first 24 hours
+        if (overstayMinutes <= 0) {
+            return 0.0;
+        }
+        return 50.0;
     }
+
     @Override
-    public String getSchemeName() { return "Fixed (RM 50)"; }
+    public String getSchemeName() {
+        return "Fixed (RM 50)";
+    }
 }
